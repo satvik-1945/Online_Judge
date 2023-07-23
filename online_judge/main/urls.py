fc29ed14d8f135fd1d3c31d51475d30f8908200c
satvik <satviktejas@gmail.com>
2023-07-23 06:49:28 +0530

    22July 11:59PM

--- online_judge/main/urls.py
+++ online_judge/main/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.HomePage,name= 'home'),      
+    path('', views.HomePage,name= 'home'),
+    path('problem/<int:problem_id>', views.problem_description,name= 'problem_description'),   
+    path('problem/<int:problem_id>/verdict', views.submit_code,name="verdict"),
 ]
--- online_judge/main/views.py
+++ online_judge/main/views.py
@@ -3,10 +3,20 @@ from django.contrib.auth.models import User
 from django.contrib.auth import login,authenticate,logout
 from django.contrib.auth.decorators import login_required
 
-from .models import problem
-# Create your views here.
-@login_required(login_url = 'login')
+# from django.shortcuts import render
+from .models import problem  # Import your 'Problem' model from models.py
+
 def HomePage(request):
     probs = problem.objects.all()
-    return render(request,'home.html',{'probs':probs})  
-    
+    return render(request, 'home.html', {'probs': probs})
+
+def problem_description(request, problem_id):
+    # Assuming you have a 'problem_id' parameter to fetch the specific problem
+    prob = problem.objects.get(pk=problem_id)
+    print(prob)
+    return render(request, 'description.html', {'prob': prob})
+
+
+def submit_code(request, problem_id):
+    prob = problem.objects.get(pk=problem_id)
+    return render(request, 'verdict.html', {'prob': prob})
